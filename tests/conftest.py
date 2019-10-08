f3486a6cdf81217cf4918320bf492f960c8bce5a
Matin Tamizi <matin@cuenca.com>
2019-10-08 15:07:19 -0500

    add vcr

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,34 @@
+import json
+from json import JSONDecodeError
+
 import pytest
 
 from mati import Client
+
+
+def scrub_access_token(response: dict) -> dict:
+    try:
+        resp = json.loads(response['body']['string'])
+    except JSONDecodeError:
+        pass
+    else:
+        if 'access_token' in resp:
+            resp['access_token'] = 'ACCESS_TOKEN'
+            user = resp['payload']['user']
+            user['_id'] = 'ID'
+            user['firstName'] = 'FIRST_NAME'
+            user['lastName'] = 'LAST_NAME'
+            resp['payload']['user'] = user
+            response['body']['string'] = json.dumps(resp)
+    return response
+
+
+@pytest.fixture(scope='module')
+def vcr_config() -> dict:
+    config = dict()
+    config['filter_headers'] = [('Authorization', None)]
+    config['before_record_response'] = scrub_access_token
+    return config
 
 
 @pytest.fixture
--- tests/resources/test_access_tokens.py
+++ tests/resources/test_access_tokens.py
@@ -1,7 +1,10 @@
+import pytest
+
 from mati import Client
 
 
-def test_create(client: Client):
+@pytest.mark.vcr
+def test_create_access_token(client: Client):
     at = client.access_tokens.create()
     assert not at.expired
     assert str(at).startswith('Bearer ')
