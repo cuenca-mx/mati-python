f853d39237232ae95da0afb8d936da673b9d63e3
Matin Tamizi <matin@cuenca.com>
2019-10-09 01:24:13 -0500

    don't need to reference the client

--- tests/resources/test_user_verification_data.py
+++ tests/resources/test_user_verification_data.py
@@ -1,16 +1,14 @@
 import pytest
 
-from mati import Client
 from mati.resources import Identity
 
 
 @pytest.mark.vcr
-def test_user_verification(client: Client, identity: Identity):
-    with open('tests/images/ine.jpg', 'rb') as ff:
-        user_validation = client.user_validation_data.create(
-            identity_id=identity._id,
+def test_user_verification(identity: Identity):
+    with open('tests/images/ine.jpg', 'rb') as ine:
+        user_validation = identity.upload_validation_data(
             filename='ine.jpg',
-            content=ff,
+            content=ine,
             input_type='document-photo',
             validation_type='national-id',
             country='MX',
