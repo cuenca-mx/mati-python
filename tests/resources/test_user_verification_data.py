260fd0a387f84d4edc6bfbb31c2ec79526baf1ad
Matin Tamizi <matin@cuenca.com>
2019-10-09 01:57:53 -0500

    define file path

--- tests/resources/test_user_verification_data.py
+++ tests/resources/test_user_verification_data.py
@@ -1,11 +1,18 @@
+import os
+
 import pytest
 
 from mati.resources import Identity
 
+FIXTURE_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'user_documentation_files'
+)
+
 
 @pytest.mark.vcr
 def test_user_verification(identity: Identity):
-    with open('tests/images/ine.jpg', 'rb') as ine:
+    filepath = os.path.join(FIXTURE_DIR, 'ine.jpg')
+    with open(filepath, 'rb') as ine:
         uploaded = identity.upload_validation_data(
             filename='ine.jpg',
             content=ine,
