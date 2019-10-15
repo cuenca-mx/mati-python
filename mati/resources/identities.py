8ba5d323040f59813f04230cfb5d765b41c523d2
Matin Tamizi <matin@cuenca.com>
2019-10-15 08:19:11 -0500

    Identity._id => Identity.id

--- mati/resources/identities.py
+++ mati/resources/identities.py
@@ -15,7 +15,7 @@ class Identity(Resource):
 
     _endpoint: ClassVar[str] = '/v2/identities'
 
-    _id: str
+    id: str
     dateCreated: dt.datetime
     dateUpdated: dt.datetime
     alive: Optional[bool]
@@ -29,20 +29,22 @@ class Identity(Resource):
     @classmethod
     def create(cls, **metadata) -> 'Identity':
         resp = cls._client.post(cls._endpoint, json=dict(metadata=metadata))
+        resp['id'] = resp.pop('_id')
         return cls(**resp)
 
     @classmethod
     def retrieve(cls, identity_id: str) -> 'Identity':
         endpoint = f'{cls._endpoint}/{identity_id}'
         resp = cls._client.get(endpoint)
+        resp['id'] = resp.pop('_id')
         return cls(**resp)
 
     def refresh(self):
-        identity = self.retrieve(self._id)
+        identity = self.retrieve(self.id)
         for k, v in identity.__dict__.items():
             setattr(self, k, v)
 
     def upload_validation_data(
         self, user_validation_files: List[UserValidationFile]
     ) -> List[dict]:
-        return UserValidationData.upload(self._id, user_validation_files)
+        return UserValidationData.upload(self.id, user_validation_files)
