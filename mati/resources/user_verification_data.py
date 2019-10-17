92f72b826abd62894b6edaf976a109e3fb70229d
Matin Tamizi <matin@cuenca.com>
2019-10-17 13:50:30 -0500

    make cleaner

--- mati/resources/user_verification_data.py
+++ mati/resources/user_verification_data.py
@@ -1,12 +1,13 @@
 import json
-from typing import Any, ClassVar, Dict, List
+from typing import Any, BinaryIO, ClassVar, Dict, List, Tuple
 
 from mati.types import UserValidationFile, ValidationInputType
 
 from .base import Resource
 
 
-def get_file_type(input_type: str) -> str:
+def get_file_type(file: UserValidationFile) -> str:
+    input_type = file.input_type
     if input_type == 'selfie-video':
         file_type = 'video'
     elif input_type == 'selfie-photo':
@@ -23,38 +24,42 @@ class UserValidationData(Resource):
 
     _endpoint: ClassVar[str] = '/v2/identities/{identity_id}/send-input'
 
+    @staticmethod
+    def _append_file(
+        files_metadata: List[Dict[str, Any]], file: UserValidationFile
+    ):
+        if file.input_type != ValidationInputType.selfie_video:
+            files_metadata.append(
+                dict(
+                    inputType=file.input_type,
+                    group=file.group,
+                    data=dict(
+                        type=file.validation_type,
+                        country=file.country,
+                        page=file.page,
+                        filename=file.filename,
+                        region=file.region,
+                    ),
+                )
+            )
+        else:
+            files_metadata.append(
+                dict(
+                    inputType=file.input_type,
+                    data=dict(filename=file.filename),
+                )
+            )
+
     @classmethod
     def upload(
         cls, identity_id: str, user_validation_files: List[UserValidationFile]
     ) -> List[Dict[str, Any]]:
         endpoint = cls._endpoint.format(identity_id=identity_id)
-        files_metadata = []
-        files_with_types = []
+        files_metadata: List[Dict[str, Any]] = []
+        files_with_types: List[Tuple[str, BinaryIO]] = []
         for file in user_validation_files:
-            if file.input_type != ValidationInputType.selfie_video:
-                files_metadata.append(
-                    dict(
-                        inputType=file.input_type,
-                        group=file.group,
-                        data=dict(
-                            type=file.validation_type,
-                            country=file.country,
-                            page=file.page,
-                            filename=file.filename,
-                            region=file.region,
-                        ),
-                    )
-                )
-            else:
-                files_metadata.append(
-                    dict(
-                        inputType=file.input_type,
-                        data=dict(filename=file.filename),
-                    )
-                )
-            files_with_types.append(
-                (get_file_type(file.input_type), file.content)
-            )
+            cls._append_file(files_metadata, file)
+            files_with_types.append((get_file_type(file), file.content))
         resp = cls._client.post(
             endpoint,
             data=dict(inputs=json.dumps(files_metadata)),
