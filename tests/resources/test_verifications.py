ce84ff4a54d035cbb8164c8e84980df6c22233fe
AlexViquez <alexviquez22@gmail.com>
2022-03-01 18:11:56 -0600

    adding coverage test

--- tests/resources/test_verifications.py
+++ tests/resources/test_verifications.py
@@ -20,3 +20,19 @@ def test_retrieve_full_verification(client: Client):
         verification.proof_of_residency_document.address
         == 'Varsovia 36, 06600 CDMX'
     )
+
+
+@pytest.mark.vcr
+def test_verification_without_liveness(client: Client):
+    verification = client.verifications.retrieve('5d9fb1f5bfbfac001a349bfb')
+    verification.steps = []
+    assert not verification.proof_of_life_document
+
+
+
+@pytest.mark.vcr
+def test_verification_without_poa(client: Client):
+    verification = client.verifications.retrieve('5d9fb1f5bfbfac001a349bfb')
+    verification.documents = [verification.documents[0]]
+    assert not verification.proof_of_residency_document
+    assert not verification.proof_of_residency_validation
