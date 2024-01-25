f18557e2bbab29195cc6c5ca2944edac785215bb
AlexViquez <alexviquez22@gmail.com>
2024-01-25 12:54:54 -0600

    tested

--- tests/test_types.py
+++ tests/test_types.py
@@ -34,7 +34,13 @@ def test_document_type(verification_document, expected_type):
     assert verification_document.document_type == expected_type
 
 
-def test_excess_fields():
+def test_from_dict():
     data = {'some': 'data', 'aditional': 'data', 'id': 'foo', 'status': 10}
     step = VerificationDocumentStep._from_dict(data)
     assert step
+
+
+def test_excess_fields():
+    data = {'some': 'data', 'aditional': 'data', 'id': 'foo', 'status': 10}
+    VerificationDocumentStep._filter_excess_fields(data)
+    assert 'some' not in data
