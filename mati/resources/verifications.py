a08f78fb32d47c47650c41ff801fc48a8afd4e13
Alex Viquez <alejandro@cuenca.com>
2024-02-07 17:25:18 -0600

    Liveness (#120)
    * adding from_dict liveness
    
    * fix
    
    * from dict
    
    * test
    
    * fixed
    
    * complete
    
    * fix
    
    * corrected version

--- mati/resources/verifications.py
+++ mati/resources/verifications.py
@@ -31,15 +31,13 @@ class Verification(Resource):
 
     def __post_init__(self):
         docs = []
-        self.steps = [
-            Liveness(**step) for step in self.steps if step['id'] == 'liveness'
-        ]
+        self.steps = [Liveness._from_dict(step) for step in self.steps]
         for doc in self.documents:
             doc['steps'] = [
                 VerificationDocumentStep._from_dict(step)
                 for step in doc['steps']
             ]
-            docs.append(VerificationDocument(**doc))
+            docs.append(VerificationDocument._from_dict(doc))
         self.documents = docs
 
     @classmethod
